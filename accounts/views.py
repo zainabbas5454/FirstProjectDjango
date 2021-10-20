ed60bf2cf81ed8a40d44b2984c92080ef4d28610
zainabbas5454 <79184461+zainabbas5454@users.noreply.github.com>
2021-10-20 16:49:26 +0500

    user login and logout
    user login and logout

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.http.response import HttpResponse
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User, auth
@@ -16,10 +17,12 @@ def register(request):
        # print(first_name,last_name,username,email)
         if password1 == password2:
             if User.objects.filter(username = username).exists():
-                print('Username already Taken')
+                messages.info(request, 'Username already taken')
+                #print('Username already Taken')
                 return redirect('register')
             elif User.objects.filter(email = email).exists():
-                print('This email already taken')
+                messages.info(request,'This email is already taken')
+                #print('This email already taken')
                 return redirect('register')
             else:
                 
@@ -28,7 +31,29 @@ def register(request):
                 print('User Created Successfully')
                 return redirect('/')
         else:
-            print('Password does not match')
+            messages.info(request,'Password does not match')
+            #print('Password does not match')
             return redirect('register')
     else:
         return render(request,'register.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = auth.authenticate(username = username, password = password)
+        
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request,'Invalid Characters')
+    
+    else:
+        return render(request,'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('login')
+    
